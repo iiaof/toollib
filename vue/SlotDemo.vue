89ee86c34d5a2ef4ff22b22d8ab6b5c4fe619a89
iiaof <60874735+iiaof@users.noreply.github.com>
2020-04-01 15:34:13 +0800

    Create SlotDemo.vue

---
+++ vue/SlotDemo.vue
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SlotDemo</title>
+</head>
+<body>
+    <div id="app">
+        
+    </div>
+</body>
+<script src="./vue.min.js"></script>
+<script>
+
+    new Vue({
+        el: '#app',
+        data: {
+            
+        },
+        components: {
+            
+        },
+        methods: {
+            
+        }
+    })
+
+</script>
+</html>
