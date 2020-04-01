e5bfd6b239e0d1fef743237b96fc7a24f8fa1a64
iiaof <60874735+iiaof@users.noreply.github.com>
2020-04-01 17:26:22 +0800

    Update and rename SlotDemo.vue to ScopedSlotDemo.vue

---
+++ vue/ScopedSlotDemo.vue
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ScopedSlotDemo</title>
+</head>
+<body>
+    <div id="app">
+        <slot-component :url="website.url">
+            <template v-slot="slotProps">
+                {{slotProps.data.title}}
+            </template>
+        </slot-component>
+    </div>
+    <template id="cpn">
+          <a :href="url">
+            <solt :data="website">
+              {{website.title}}
+            </solt>
+          </a>
+    </template>
+</body>
+<script src="./vue.min.js"></script>
+<script>
+    const SlotComponent = {
+        template: '#cpn',
+        props: {
+            url: {
+                type: String,
+                default: ''
+            }
+        },
+        data() {
+            return {
+                website: {
+                    url: 'http://child.com/',
+                    title: 'child_title'
+                }
+            }
+        }
+    };
+
+    new Vue({
+        el: '#app',
+        data: {
+            name: 'parent',
+            website: {
+                url: 'http://parent.com',
+                title: 'parent_title'
+            }
+        },
+        components: {
+            SlotComponent
+        }
+    })
+
+</script>
+</html>
